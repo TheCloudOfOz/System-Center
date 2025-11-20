459d9904f655b82f5a5198c4ea19995f0a173a8f
HARRBY <160479232+HarrbyPtyLtd@users.noreply.github.com>
2025-11-21 05:10:49 +1100

    Refactor and enhance discovery methods in Lab02.ps1

--- Lab02.ps1
+++ Lab02.ps1
@@ -7,6 +7,7 @@ Get-CMDiscoveryMethod | Where-Object { $_.ComponentName -like "SMS_AD*" } | Sele
         }
     }
 }
+
 $14days=New-CMSchedule -RecurCount 14 -RecurInterval Days
 Set-CMDiscoveryMethod -ActiveDirectoryForestDiscovery -Enabled $true -EnableActiveDirectorySiteBoundaryCreation $true -PollingSchedule $14days -SiteCode RDU
 (Get-CMDiscoveryMethod -Name ActiveDirectoryForestDiscovery).Properties.Props
@@ -14,6 +15,7 @@ Set-CMDiscoveryMethod -ActiveDirectoryForestDiscovery -Enabled $true -EnableActi
 Convert-CMSchedule -ScheduleString "0001200000100070"
 Invoke-CMForestDiscovery
 Get-CMBoundary
+
 Set-CMDiscoveryMethod -ActiveDirectorySystemDiscovery -Enabled $true -AddActiveDirectoryContainer "ldap://ou=rdu clients,dc=mts,dc=com","ldap://ou=rdu servers,dc=mts,dc=com" -SiteCode RDU -EnableDeltaDiscovery $true -DeltaDiscoveryIntervalMins 15 -PollingSchedule $14days -AddAdditionalAttribute Department
 (Get-CMDiscoveryMethod -Name ActiveDirectorySystemDiscovery).EmbeddedPropertyLists."AD     Containers"
 (Get-CMDiscoveryMethod -Name ActiveDirectorySystemDiscovery).Properties.Props | Where-Object PropertyName -eq "Startup Schedule"
@@ -23,6 +25,7 @@ Convert-CMSchedule -ScheduleString "0001200000100070"
 Invoke-CMSystemDiscovery -SiteCode RDU
 Get-CMDevice | Select-Object -Property Name
 $UserSched=New-CMSchedule -RecurCount 7 -RecurInterval Days
+
 Set-CMDiscoveryMethod -ActiveDirectoryUserDiscovery -Enabled $true -AddActiveDirectoryContainer "ldap://ou=rdu users,dc=mts,dc=com","ldap://ou=clt users,dc=mts,dc=com" -SiteCode RDU -EnableDeltaDiscovery $true -DeltaDiscoveryMins 10 -PollingSchedule $UserSched -AddAdditionalAttribute Department,Division
 (Get-CMDiscoveryMethod -Name ActiveDirectoryUserDiscovery).EmbeddedPropertyLists."AD Containers"
 (Get-CMDiscoveryMethod -Name ActiveDirectoryUserDiscovery).Properties.Props | Where-Object PropertyName -eq "Startup Schedule"
@@ -33,9 +36,11 @@ Invoke-CMUserDiscovery -SiteCode RDU
 Get-CMUser | Select-Object -Property Name
 $21days=New-CMSchedule -RecurInterval Days -RecurCount 21
 $30days=New-CMSchedule -RecurInterval Days -RecurCount 30
+
 New-CMDeviceCollection -LimitingCollectionName "All Systems" -Name "All Clients" -RefreshSchedule $21days -RefreshType Both
 New-CMDeviceCollection -LimitingCollectionName "All Clients" -Name "RDU Clients" -RefreshSchedule $21days -RefreshType Both
 New-CMDeviceCollection -LimitingCollectionName "All Systems" -Name "CM Servers" -RefreshSchedule $30days -RefreshType Periodic
+
 Add-CMDeviceCollectionQueryMembershipRule -CollectionName "All Clients" -QueryExpression "select * from SMS_R_System where SMS_R_System.OperatingSystemNameandVersion like 'Microsoft Windows NT Workstation %'" -RuleName "All Clients"
 Add-CMDeviceCollectionQueryMembershipRule -CollectionName "RDU Clients" -QueryExpression "select * from SMS_R_System where SMS_R_System.SystemOUName = 'MTS.COM/RDU Clients'" -RuleName "RDU Clients"
 Add-CMDeviceCollectionDirectMembershipRule -CollectionName "CM Servers" -Resource (Get-CMDevice -Name "RDU-CM-01")
@@ -58,3 +63,4 @@ Get-CMBoundary
 Get-CMBoundary | Select-Object DisplayName,-Value
 Get-CMBoundaryGroup
 Get-CMBoundaryGroup | Select-Object Name,DefaultSiteCode,MemberCount,SiteSystemCount
+
